fe7172f4feacdffd0961521784b51277d1e8b96b
Cris <cristiansouza.agra@gmail.com>
2024-06-04 17:42:48 -0300

    Finalizando ajustes visuais de pedido realizado

--- js/modules/order/showOrderPlaced.js
+++ js/modules/order/showOrderPlaced.js
@@ -6,9 +6,19 @@ export function ShowOrderPlaced(responseOrder) {
     imgOrder.src = responseOrder.image;
     imgOrder.alt = responseOrder.description;
 
+
+
+    const screenWidth = window.innerWidth || document.documentElement.clientWidth;
+    let marginTop;
+
+    if (screenWidth < 1024) {
+        marginTop = '-1.3rem';
+    } else {
+        marginTop = '1rem';
+    }
     const title = document.createElement('h3');
     title.classList.add('m-plus-rounded-1c-black', 'text-white');
-    title.style.marginTop = '-4.5rem';
+    title.style.marginTop = marginTop;
     title.textContent = 'Your Order:';
 
     const orderDescription = document.createElement('h2');
